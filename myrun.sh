7175d70a06807cdb08a91369fd71aed1fd0796d8
Italo Valcy S Brito <italovalcy@gmail.com>
2020-09-12 01:29:15 +0000

    adding script to run experiments

--- myrun.sh
+++ myrun.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
 MAX_RUN=12
-TRACE=trace/scenario-20_DDSN.ns_movements
+#TRACE=trace/scenario-20_DDSN.ns_movements
+TRACE=trace/scenario-20nodes-RPGM-500x500.ns_movements
 SCENARIO=ndncomm2020-exp1
+
+run_scenario() {
+    local i=$1
+    local RESULT_DIR=results/run-rpgm-$i
+    rm -rf $RESULT_DIR/
+    mkdir -p $RESULT_DIR
+    local TIME=$(date +%s)
+    echo "running $SCENARIO run=$i..."
+    NS_LOG=ndn.Ndvr:ndn-cxx.nfd.Forwarder ./waf --run "$SCENARIO --wifiRange=60 --traceFile=$TRACE --syncDataRounds=10" > $RESULT_DIR/$SCENARIO.log 2>&1
+    local TIME2=$(date +%s)
+    echo "--> done run=$i duration=$((TIME2 - TIME))s"
+}
 
 ./waf
 for i in `seq 1 $MAX_RUN`; do
-    RESULT_DIR=results/run-$i
-    rm -rf $RESULT_DIR/
-    mkdir -p $RESULT_DIR
-    TIME=$(date +%s)
-    echo "running $SCENARIO run=$i..."
-    NS_LOG=ndn.Ndvr:ndn-cxx.nfd.Forwarder ./waf --run "$SCENARIO --wifiRange=60 --traceFile=$TRACE --syncDataRounds=10" > $RESULT_DIR/$SCENARIO.log 2>&1
-    TIME2=$(date +%s)
-    echo "--> done run=$i duration=$((TIME2 - TIME))s"
-    sleep 5
+    run_scenario $i
+    sleep 10
 done
