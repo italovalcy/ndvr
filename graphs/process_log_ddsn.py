68196052d6c102d53f7e5547a84208a9c0ea38a5
Italo Valcy S Brito <italovalcy@gmail.com>
2020-09-12 01:04:49 +0000

    adding parameters to process_log scripts

--- graphs/process_log_ddsn.py
+++ graphs/process_log_ddsn.py
@@ -4,10 +4,17 @@ import os
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
 
-if len(sys.argv) < 3:
-    print "USAGE %s <FILENAME> <NUM_NODES>" % (sys.argv[0])
-    sys.exit(0)
+parser = argparse.ArgumentParser(description='Process log file for DDSN to calculate data retrieval delay')
+parser.add_argument('--file_name', metavar='FILE', required=True,
+                    help='filename containing the logs (NS_LOG=nfd.Forwarder) for DDSN')
+parser.add_argument('--num_nodes', type=int, required=True,
+                    help='Number of nodes')
+parser.add_argument('--output', metavar='FILE', required=True,
+                    help='Filename to save the results')
+
+args = parser.parse_args()
 
 class DataInfo:
     def __init__(self, adv_time):
@@ -15,14 +22,11 @@ class DataInfo:
         self.count = 0
         self.last_seen = adv_time
 
-file_name = sys.argv[1]
-num_nodes = int(sys.argv[2])
-
 data_store = {}
 sync_duration = []
 sync_duration_all = []
 
-file = open(file_name)
+file = open(args.file_name)
 for line in file:
     if line.find("Store New Data:") != -1:
         elements = line.split(' ')
@@ -35,16 +39,16 @@ for line in file:
             data_store[data_name].last_seen = time
         data_info = data_store[data_name]
         # sanity check: the number of data sync cannot be greater than num_nodes-1
-        assert data_info.count <= num_nodes - 1
+        assert data_info.count <= args.num_nodes - 1
         # individual sync duration
         sync_duration.append(data_info.last_seen - data_info.adv_time)
         # total sync duration (we care about 90% of the nodes, just as DDSN)
-        if data_info.count == int(0.9*num_nodes):
+        if data_info.count == int(0.9*args.num_nodes):
             sync_duration_all.append(data_info.last_seen - data_info.adv_time)
 
 print "data sync duration (mean) = " + str(np.mean(sync_duration_all))
 import json
-with open('/tmp/data_ddsn', "w") as f:
+with open(args.output, "w") as f:
     f.write(json.dumps(sync_duration))
 #sorted_data = np.sort(sync_duration)
 #yvals=np.arange(len(sorted_data))/float(len(sorted_data)-1)
