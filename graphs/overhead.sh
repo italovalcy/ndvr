0dc9ed27802c3ed18ece1a0dd8321a3e4d984f41
Italo Valcy S Brito <italovalcy@gmail.com>
2020-09-13 02:28:25 +0000

    adjusting overhead script to accept the strategy as parameter

--- graphs/overhead.sh
+++ graphs/overhead.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-fgrep "ndn-cxx.nfd.Forwarder:onOutgoingInterest(): [DEBUG] onOutgoingInterest out=(257,0) interest=/localhop/ndvr/ehlo/" results/ndncomm2020.log > /tmp/ndvr-nfd-ehlo-interest
-fgrep "ndn-cxx.nfd.Forwarder:onOutgoingInterest(): [DEBUG] onOutgoingInterest out=(257,0) interest=/localhop/ndvr/dvinfo/" results/ndncomm2020.log > /tmp/ndvr-nfd-dvinfo-interest
-fgrep "ndn-cxx.nfd.Forwarder:onOutgoingData(): [DEBUG] onOutgoingData out=(257,0) data=/localhop/ndvr/dvinfo/" results/ndncomm2020.log > /tmp/ndvr-nfd-dvinfo-data
+usage() {
+    echo "Usage: $0 <RUN> {ndvr|ddsn}"
+    exit 0
+}
 
-fgrep "nfd.Forwarder:onOutgoingInterest(): [DEBUG] onOutgoingInterest face=256 interest=/ndn/syncNotify/" ~/DDSN-Simulation/DDSN/result/1/raw/loss_rate_0.0-new.txt > /tmp/nfd-ddsn-sync-interest
-fgrep "nfd.Forwarder:onOutgoingData(): [DEBUG] onOutgoingData face=256 data=/ndn/syncNotify/" ~/DDSN-Simulation/DDSN/result/1/raw/loss_rate_0.0-new.txt > /tmp/nfd-ddsn-sync-ack
+RUN=$1
+APP=$2
 
-echo "NDVR:"
-wc -l /tmp/ndvr-nfd-*
-
-echo "DDSN:"
-wc -l /tmp/nfd-ddsn-*
+case "$APP" in
+    ndvr)
+        fgrep "ndn-cxx.nfd.Forwarder:onOutgoingInterest(): [DEBUG] onOutgoingInterest out=(257,0) interest=/localhop/ndvr/ehlo/"   results/run-$RUN/ndncomm2020-exp1.log > results/overhead-ndvr-ehlointers-$RUN
+        fgrep "ndn-cxx.nfd.Forwarder:onOutgoingInterest(): [DEBUG] onOutgoingInterest out=(257,0) interest=/localhop/ndvr/dvinfo/" results/run-$RUN/ndncomm2020-exp1.log > results/overhead-ndvr-dvinfointe-$RUN
+        fgrep "ndn-cxx.nfd.Forwarder:onOutgoingData(): [DEBUG] onOutgoingData out=(257,0) data=/localhop/ndvr/dvinfo/"             results/run-$RUN/ndncomm2020-exp1.log > results/overhead-ndvr-dvinfodata-$RUN
+        echo "Overhead NDVR $RUN:"
+        wc -l results/overhead-ndvr-*-$RUN
+        ;;
+    ddsn)
+        fgrep "nfd.Forwarder:onOutgoingInterest(): [DEBUG] onOutgoingInterest face=256 interest=/ndn/syncNotify/" ~/DDSN-Simulation/DDSN/result/$RUN/raw/loss_rate_0.0-new.txt > results/overhead-ddsn-syncint-$RUN
+        fgrep "nfd.Forwarder:onOutgoingData(): [DEBUG] onOutgoingData face=256 data=/ndn/syncNotify/"             ~/DDSN-Simulation/DDSN/result/$RUN/raw/loss_rate_0.0-new.txt > results/overhead-ddsn-syncack-$RUN
+        echo "Overhead DDSN $RUN:"
+        wc -l results/overhead-ddsn-*-$RUN
+        ;;
+    *)
+        usage
+        ;;
+esac
