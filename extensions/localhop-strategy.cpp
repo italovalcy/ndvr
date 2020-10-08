19faf2e5a58e3cec57a74c208dc5548f45457cae
Italo Valcy S Brito <italovalcy@gmail.com>
2020-10-08 13:32:42 +0000

    enabling localhop-strategy

---
+++ extensions/localhop-strategy.cpp
@@ -0,0 +1,84 @@
+#include "localhop-strategy.hpp"
+#include "common/logger.hpp"
+
+namespace nfd {
+namespace fw {
+
+NFD_REGISTER_STRATEGY(LocalhopStrategy);
+
+NFD_LOG_INIT(LocalhopStrategy);
+
+const time::milliseconds LocalhopStrategy::RETX_SUPPRESSION_INITIAL(10);
+const time::milliseconds LocalhopStrategy::RETX_SUPPRESSION_MAX(250);
+
+LocalhopStrategy::LocalhopStrategy(Forwarder& forwarder, const Name& name)
+  : Strategy(forwarder)
+  , m_retxSuppression(RETX_SUPPRESSION_INITIAL,
+                      RetxSuppressionExponential::DEFAULT_MULTIPLIER,
+                      RETX_SUPPRESSION_MAX)
+{
+  this->setInstanceName(makeInstanceName(name, getStrategyName()));
+}
+
+const Name&
+LocalhopStrategy::getStrategyName()
+{
+  static Name strategyName("/localhost/nfd/strategy/localhop/%FD%01");
+  return strategyName;
+}
+
+bool
+my_wouldViolateScope(const Face& inFace, const Interest& interest, const Face& outFace)
+{
+  if (outFace.getScope() == ndn::nfd::FACE_SCOPE_LOCAL) {
+    // forwarding to a local face is always allowed
+    return false;
+  }
+
+  // localhop Interests can be forwarded to a non-local face only if it comes from a local face
+  return inFace.getScope() != ndn::nfd::FACE_SCOPE_LOCAL;
+}
+
+void
+LocalhopStrategy::afterReceiveInterest(const FaceEndpoint& ingress, const Interest& interest,
+                                        const shared_ptr<pit::Entry>& pitEntry)
+{
+  const fib::Entry& fibEntry = this->lookupFib(*pitEntry);
+  const fib::NextHopList& nexthops = fibEntry.getNextHops();
+  NFD_LOG_DEBUG("I: " << interest << " inFaceId=" << ingress.face.getId());
+
+  int nEligibleNextHops = 0;
+
+  bool isSuppressed = false;
+
+  for (const auto& nexthop : nexthops) {
+    Face& outFace = nexthop.getFace();
+
+    RetxSuppressionResult suppressResult = m_retxSuppression.decidePerUpstream(*pitEntry, outFace);
+
+    if ((outFace.getId() == ingress.face.getId() && outFace.getLinkType() != ndn::nfd::LINK_TYPE_AD_HOC) ||
+        my_wouldViolateScope(ingress.face, interest, outFace)) {
+      continue;
+    }
+
+    if (suppressResult == RetxSuppressionResult::SUPPRESS) {
+      NFD_LOG_DEBUG(interest << " from=" << ingress << " to=" << outFace.getId() << " suppressed");
+      isSuppressed = true;
+      continue;
+    }
+
+    this->sendInterest(pitEntry, FaceEndpoint(outFace, 0), interest);
+    NFD_LOG_DEBUG(interest << " from=" << ingress << " pitEntry-to=" << outFace.getId());
+
+    if (suppressResult == RetxSuppressionResult::FORWARD) {
+      m_retxSuppression.incrementIntervalForOutRecord(*pitEntry->getOutRecord(outFace));
+    }
+    ++nEligibleNextHops;
+  }
+  if (nEligibleNextHops == 0 && !isSuppressed) {
+    NFD_LOG_DEBUG(interest << " from=" << ingress << " noNextHop: rejectPendingInterest");
+    this->rejectPendingInterest(pitEntry);
+  }
+}
+} // namespace fw
+} // namespace nfd
