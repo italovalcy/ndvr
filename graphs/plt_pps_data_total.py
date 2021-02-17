2856edc3bddca5fd10c85e09e9c4b4746b4dd742
Italo Valcy S Brito <italovalcy@gmail.com>
2021-02-17 00:28:27 +0000

    adjust plt script for pps data/total

--- graphs/plt_pps_data_total.py
+++ graphs/plt_pps_data_total.py
@@ -3,6 +3,7 @@
 import sys
 import json
 import numpy as np
+from scipy.stats import sem, t
 import matplotlib.pyplot as plt
 import argparse
 
@@ -50,21 +51,21 @@ for i in range(1, args.seconds):
         try:
             mydata = data[s][idx]['data']
             mean_data = np.mean(mydata)
-            #intv_data_v = sem(mydata) * t.ppf((1 + confidence) / 2, len(mydata) - 1)
+            intv_data_v = sem(mydata) * t.ppf((1 + confidence) / 2, len(mydata) - 1)
         except:
             mean_data = 0
             intv_data_v = 0
         try:
             mydata = data[s][idx]['total']
             mean_total = np.mean(mydata)
-            #intv_total_v = sem(mydata) * t.ppf((1 + confidence) / 2, len(mydata) - 1)
+            intv_total_v = sem(mydata) * t.ppf((1 + confidence) / 2, len(mydata) - 1)
         except:
             mean_total = 0
             intv_total_v = 0
         result_data[s].append(mean_data)
-        #intv_data[s].append(intv_data_v)
+        intv_data[s].append(intv_data_v)
         result_total[s].append(mean_total)
-        #intv_total[s].append(intv_total_v)
+        intv_total[s].append(intv_total_v)
 
 width=2.0
 #plt.figure(figsize=(20,10))
@@ -74,7 +75,7 @@ axs[1].set_title("Total (pps)")
 
 for s in strategies:
     axs[0].plot(range(1, args.seconds), result_data[s],  label = s, linewidth=width)
-    #axs[0].fill_between(range(1, args.seconds), result_data[s] - intv_data[s],  result_data[s] + intv_data[s])
+    #axs[0].fill_between(range(1, args.seconds), np.array(result_data[s]) - np.array(intv_data[s]),  np.array(result_data[s]) + np.array(intv_data[s]))
     axs[1].plot(range(1, args.seconds), result_total[s],  label = s, linewidth=width)
 fig.legend(loc = 'lower right')
 plt.show()
